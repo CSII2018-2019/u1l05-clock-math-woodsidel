5a446a9b762d2ec4a2863f83154d95ad0afd346b
woodsidel <woodsidel@LR064WA7-1718.ellis.int>
2018-10-08 16:00:26 -0400

    The hours and minutes don't print

--- src/U1L05ClockMath.java
+++ src/U1L05ClockMath.java
@@ -6,20 +6,31 @@ public class U1L05ClockMath {
 		//give an ending time
 		
 		//get start time as a string
-		String answer = JOptionPane.showInputDialog(null, "Give me a start time (ex: 3:10)");
+		String startTime = JOptionPane.showInputDialog(null, "Give me a start time (ex: 3:10)");
+		String endTime = JOptionPane.showInputDialog(null, "Give me an end time (ex: 3:10)");
 		//figure out where the colon in
-		int colonInd = answer.indexOf(':');
+		int colonInd = startTime.indexOf(':');
+		int indColon = endTime.indexOf(':');
 		//take start of string to where the colon is and make it an integer
-		int hours = Integer.parseInt(answer.substring(0, colonInd));
+		int startHours = Integer.parseInt(startTime.substring(0, colonInd));
+		int endHours = Integer.parseInt(endTime.substring(0, indColon));
 		//take colon to end of string and make it an integer
-		int minutes = Integer.parseInt(answer.substring(colonInd + 1, answer.length()));
+		int startMinutes = Integer.parseInt(startTime.substring(colonInd + 1, startTime.length()));
+		int endMinutes = Integer.parseInt(endTime.substring(colonInd + 1, endTime.length()));
 		//check that it came in correctly
-		//System.out.println("Hours: " + hours);
-		//System.out.println("Minutes: " + minutes);
+		if(endHours<startHours) {
+			endHours += 12;
+		}
+		System.out.println("Hours: " + str(endHours-startHours)+"\n Minutes: " + str(endMinutes-startMinutes));
 		
 		
 		
 		
 		
+	}
+
+	private static String str(int i) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
